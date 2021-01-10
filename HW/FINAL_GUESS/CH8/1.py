cadbc88aee08cb7aed2dde0e9746cf8cd1d65225
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2021-01-10 15:21:21 +0800

    update

---
+++ HW/FINAL_GUESS/CH8/1.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+# init window
+window = tk.Tk ()
+
+# constant
+invest = 10000
+
+
+# make text area
+textField = tk.Text (window)
+textField.insert (INSERT, )
+textField.pack ()
+
+
+
+# start window
+window.mainloop ()
---
+++ HW/FINAL_GUESS/CH8/invest.py
@@ -0,0 +1,3 @@
+class Invest:
+	def __init__ (self):
+
